de4d75843409dad10387477206955902d7f4f0c0
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2025-12-12 10:25:51 +0700

    first commit

---
+++ src/HELLOFILE_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hoang
+ */
+import java.util.*;
+public class HELLOFILE_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(new File("Hello.txt"));
+        System.out.println(sc.nextLine());
+    }
+}
