0546a9bbdf441c50b73d93d9190a722ba92520a0
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2025-12-20 18:32:27 +0700

    update

---
+++ src/J01016_18.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hoang
+ */
+public class J01016_18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        String b = "" + a;
+        int sum = 0 ; 
+        int cnt4[] = new int[256];
+        int cnt7[] = new int[256];
+        int cnt = 0 ; 
+        for(int i = 0 ; i < b.length(); i++){
+            char c = b.charAt(i);
+            if(c == '4' || c == '7') sum ++;
+        }
+        if (sum == 4 || sum == 7) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
